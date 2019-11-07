c5b45616d65dd5793e06b5b4c0f1be7f1418be8a
d.burnyshev <d.burnyshev@tinkoff.ru>
2019-11-07 16:23:26 +0300

    Add global components to the project

--- src/views/404.vue
+++ src/views/404.vue
@@ -10,33 +10,33 @@
 </template>
 
 <style scoped>
-#message { 
-  background: white; 
-  max-width: 360px; 
-  margin: 10px auto 16px; 
-  padding: 32px 24px 16px; 
+#message {
+  background: white;
+  max-width: 360px;
+  margin: 10px auto 16px;
+  padding: 32px 24px 16px;
 }
-#message h3 { 
-  color: #888; 
-  font-weight: normal; 
-  font-size: 16px; 
-  margin: 16px 0 12px; 
+#message h3 {
+  color: #888;
+  font-weight: normal;
+  font-size: 16px;
+  margin: 16px 0 12px;
 }
-#message h2 { 
-  color: #ffa100; 
-  font-weight: bold; 
-  font-size: 16px; 
-  margin: 0 0 8px; 
+#message h2 {
+  color: #ffa100;
+  font-weight: bold;
+  font-size: 16px;
+  margin: 0 0 8px;
 }
-#message h1 { 
-  font-size: 22px; 
-  font-weight: 300; 
-  color: rgba(0,0,0,0.6); 
+#message h1 {
+  font-size: 22px;
+  font-weight: 300;
+  color: rgba(0, 0, 0, 0.6);
   margin: 0 0 16px;
 }
-#message p { 
-  line-height: 140%; 
-  margin: 16px 0 24px; 
-  font-size: 14px; 
+#message p {
+  line-height: 140%;
+  margin: 16px 0 24px;
+  font-size: 14px;
 }
 </style>
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,9 +1,9 @@
 <template>
-    <h1>Event showing #{{ id }}</h1>
+  <h1>Event showing #{{ id }}</h1>
 </template>
 
 <script>
-   export default {
-    props: ["id"]
-   } 
+export default {
+  props: ["id"]
+};
 </script>

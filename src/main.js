1123aa4842470ed24a5ff412d83bf719c625a60c
d.burnyshev <d.burnyshev@tinkoff.ru>
2019-11-08 16:54:44 +0300

    - add BaseIcon as Icon

--- src/main.js
+++ src/main.js
@@ -2,42 +2,28 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import upperFirst from 'lodash/upperFirst'
-import camelCase from 'lodash/camelCase'
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
 
 const requireComponent = require.context(
-  // Относительный путь до каталога компонентов
-  './components',
-  // Обрабатывать или нет подкаталоги
+  "./components",
   false,
-  // Регулярное выражение для определения файлов базовых компонентов
   /Base[A-Z]\w+\.(vue|js)$/
-)
+);
 
 requireComponent.keys().forEach(fileName => {
-  // Получение конфигурации компонента
-  const componentConfig = requireComponent(fileName)
-
-  // Получение имени компонента в PascalCase
+  const componentConfig = requireComponent(fileName);
   const componentName = upperFirst(
     camelCase(
-      // Получаем имя файла независимо от глубины вложенности
       fileName
-        .split('/')
+        .split("/")
         .pop()
-        .replace(/\.\w+$/, '')
+        .replace(/\.\w+$/, "")
     )
-  )
+  );
 
-  // Глобальная регистрация компонента
-  Vue.component(
-    componentName,
-    // Поиск опций компонента в `.default`, который будет существовать,
-    // если компонент экспортирован с помощью `export default`,
-    // иначе будет использован корневой уровень модуля.
-    componentConfig.default || componentConfig
-  )
-})
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 Vue.config.productionTip = false;
 

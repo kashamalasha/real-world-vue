4e8da1ac96f6b2d4aff4d48b64ae1fcda5d9ba6f
kashamalasha <katerinache@CheMAC-2.local>
2019-11-22 00:32:09 +0300

    Add vuex to EventCreate

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,21 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: "1", name: "Katya" },
+    categories: ["Еда", "Товары для дома", "Автотовары"],
+    todos: [
+      { id: 1, text: "test", done: true },
+      { id: 2, text: "test2", done: false },
+      { id: 3, text: "test3", done: true }
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length;
+    }
+  }
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event {{ user.name }}</h1>
+    <p>Created by {{ catLength }}</p>
+    <ul>
+      <li v-for="cat in categories" v-bind:key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapState(["user", "categories"])
+  }
+};
+</script>
